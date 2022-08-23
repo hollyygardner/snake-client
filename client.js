04a410a39d56789c596b219bd5b7d68e1e0cfcf2
Holly Gardner <hollylizgardner@gmail.com>
2022-08-22 22:56:04 -0400

    Require constants

--- client.js
+++ client.js
@@ -1,10 +1,9 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
   });
 
   // interpret incoming data as text
